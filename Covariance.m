4a9cdeb0cfb61ea9c91f6c3b746d20ca517702e3
Russell <`russjgoebel@gmail.com`>
2021-09-21 19:19:31 -0400

    First commit

---
+++ Covariance.m
@@ -0,0 +1,4 @@
+%%% Covariance Estimator %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Estimates covariance c(v_i(s),v_j(t)) given functions v_i and v_j
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
